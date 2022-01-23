11ffee311b41633f8626139dacf52ee8660dbfb3
Gaurav Chaddha <chaddhag@gmail.com>
2022-01-23 22:19:38 +0530

    Add sorting to product sheet properties mapping

--- apis/product/section/textSection.go
+++ apis/product/section/textSection.go
@@ -4,10 +4,16 @@ import (
 	"phantom/apis/product/models"
 	"phantom/dataLayer/uiModels/atoms"
 	"phantom/dataLayer/uiModels/snippets"
+	"sort"
 )
 
 func MakePropertyMappingSection(apiDbResult *models.ApiDbResult) *snippets.SnippetSectionData {
 	propertyMappingSection := snippets.MakeTextSnippetDataArrFromPropertyMapping(apiDbResult.PropertyMapping)
+	sort.SliceStable(propertyMappingSection, func(one, two int) bool {
+		textSnippetDataOne := propertyMappingSection[one]
+		textSnippetDataTwo := propertyMappingSection[two]
+		return textSnippetDataOne.Title.Text < textSnippetDataTwo.Title.Text
+	})
 	return &snippets.SnippetSectionData{
 		HeaderData: &snippets.SnippetSectionHeaderData{
 			Title: &atoms.TextData{
